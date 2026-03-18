c5e670d86b42af42955a910e42ea9d1c66c539ee
Kei-Taros <orz.www127@gmail.com>
2026-03-18 17:58:38 +0900

    feat: RouteTransitionを実装

--- src/lib/components/templates/index.ts
+++ src/lib/components/templates/index.ts
@@ -1,2 +1,3 @@
 export { default as Header } from './Header/Header.svelte';
 export { default as Footer } from './Footer/Footer.svelte';
+export { default as RouteTransition } from './RouteTransition/RouteTransition.svelte';
