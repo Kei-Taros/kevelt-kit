4e431bb203b46672d9b35050147edddc3734638f
Kei-Taros <orz.www127@gmail.com>
2026-03-24 18:49:15 +0900

    feat: Modalを実装

--- src/lib/components/Molecule/index.ts
+++ src/lib/components/Molecule/index.ts
@@ -1,4 +1,5 @@
 export { default as CardCarousel } from './CardCarousel/CardCarousel.svelte';
 export type { CardCarouselItem } from './CardCarousel/CardCarousel.types';
+export { default as Modal } from './Modal/Modal.svelte';
 export { default as WorkCard } from './WorkCard/WorkCard.svelte';
 export type { WorkCardItem } from './WorkCard/WorkCard.types';
