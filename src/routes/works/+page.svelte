3371fa416bf3b0568ab40109370fefe28bd1cfdf
Kei-Taros <orz.www127@gmail.com>
2026-03-31 12:42:59 +0900

    feat: worksPageを実装

--- src/routes/works/+page.svelte
+++ src/routes/works/+page.svelte
@@ -1,12 +1,47 @@
 <script lang="ts">
+  import { page } from '$app/state';
+  import { pushState, replaceState } from '$app/navigation';
   import { Heading } from '$lib/components/atoms';
-
-  import * as styles from './works.css';
+  import { WorkCardGrid, WorkModal } from '$lib/components/organisms';
+  import type { WorkCardItem } from '$lib/components/Molecule';
   import * as spacing from '$lib/styles/spacing.css';
   import * as layout from '$lib/styles/layout.css';
+  import * as constants from './constants/works';
+
+  const selectedWorkId = $derived(page.state.selectedWorkId ?? null);
+  const isOpen = $derived(selectedWorkId !== null);
+
+  const handleCardClick = (item: WorkCardItem) => {
+    pushState('', {
+      ...page.state,
+      selectedWorkId: item.id
+    });
+  };
+
+  const closeModal = () => {
+    replaceState('', {
+      ...page.state,
+      selectedWorkId: undefined
+    });
+  };
+
+  const selectedWorkDetail = $derived(
+    constants.WORK_DETAILS.find((detail) => detail.id === selectedWorkId) ?? null
+  );
 </script>
 
+<svelte:head>
+  <title>Works | KeveltKit</title>
+</svelte:head>
+
 <section class={layout.contentInner}>
-  <div class={spacing.mbM}><Heading label="Works" /></div>
-  <div>test</div>
+  <div class={spacing.mbM}>
+    <Heading label="Works" />
+  </div>
+
+  <WorkCardGrid items={constants.WORK_CARD} onCardClick={handleCardClick} />
+
+  {#if selectedWorkDetail}
+    <WorkModal {isOpen} {closeModal} workDetail={selectedWorkDetail} />
+  {/if}
 </section>
