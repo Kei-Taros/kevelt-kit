c39164f826b1336f09d0e176320109aa44ae6edd
Kei-Taros <orz.www127@gmail.com>
2026-04-23 01:03:47 +0900

    feat: newsPageを実装

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
   import { Heading } from '$lib/components/atoms';
-
-  import * as styles from './news.css';
+  import { LatestNewsGrid, NewsItemList } from '$lib/components/organisms';
+  import type { PageData } from './$types';
   import * as spacing from '$lib/styles/spacing.css';
   import * as layout from '$lib/styles/layout.css';
+
+  let { data }: { data: PageData } = $props();
 </script>
 
+<svelte:head>
+  <title>News | KeveltKit</title>
+</svelte:head>
+
 <section class={layout.contentInner}>
-  <div class={spacing.mbM}><Heading label="News" /></div>
-  <div>test</div>
+  <Heading label="News" />
+
+  <div class={spacing.mbXXXL}>
+    <LatestNewsGrid items={data.newsList} />
+  </div>
+  <NewsItemList items={data.newsList} />
 </section>
---
+++ src/routes/news/+page.ts
@@ -0,0 +1,37 @@
+import type { PageLoad } from './$types';
+import type { SvxModule } from '$lib/types/content.types';
+import type { NewsData } from '$lib/types/news.types';
+
+export const load: PageLoad = async () => {
+  const modules = import.meta.glob<SvxModule>('/src/content/news/**/*.svx', {
+    eager: true
+  });
+
+  const getSlugFromPath = (path: string) => path.split('/').pop()?.replace('.svx', '') ?? '';
+
+  const newsList: NewsData[] = Object.entries(modules)
+    .filter(([path, module]) => {
+      const slug = getSlugFromPath(path);
+      return module.metadata.published && slug !== 'yyyymmdd';
+    })
+    .sort(([pathA], [pathB]) => Number(getSlugFromPath(pathB)) - Number(getSlugFromPath(pathA)))
+    .map(([path, module]) => {
+      const slug = getSlugFromPath(path);
+
+      return {
+        title: module.metadata.title,
+        date: module.metadata.date,
+        href: `/news/${slug}`,
+        src: module.metadata.thumbnail
+          ? {
+              webp: `${module.metadata.thumbnail}.webp`,
+              png: `${module.metadata.thumbnail}.png`
+            }
+          : undefined
+      };
+    });
+
+  return {
+    newsList
+  };
+};
