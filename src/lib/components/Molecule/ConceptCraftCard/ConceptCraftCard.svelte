ce1830ad9d8c86dcef22f78c668200fc117f9745
Kei-Taros <orz.www127@gmail.com>
2026-05-14 22:36:08 +0900

    feat: ConceptCraftCardを実装

---
+++ src/lib/components/Molecule/ConceptCraftCard/ConceptCraftCard.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { Heading } from '$lib/components/atoms';
+  import type { ConceptCraftData } from '$lib/types/concept.types';
+  import * as spacing from '$lib/styles/spacing.css';
+  import * as styles from './ConceptCraftCard.css';
+
+  interface Props {
+    item: ConceptCraftData;
+  }
+
+  let { item }: Props = $props();
+</script>
+
+<div class={styles.card}>
+  <picture>
+    <source srcset={item.src.webp} type="image/webp" />
+    <img class={styles.cardImage} src={item.src.png} alt={item.title} />
+  </picture>
+
+  <div class={styles.cardContent}>
+    <div class={spacing.mbXXS}>
+      <Heading label={item.title} as="h4" />
+    </div>
+
+    {#each item.descriptions as description}
+      <p>{description}</p>
+    {/each}
+  </div>
+</div>
---
+++ src/lib/types/concept.types.ts
@@ -0,0 +1,7 @@
+import type { ImgSrc } from '$lib/types/image.types';
+
+export interface ConceptCraftData {
+  title: string;
+  descriptions: string[];
+  src: ImgSrc;
+}
