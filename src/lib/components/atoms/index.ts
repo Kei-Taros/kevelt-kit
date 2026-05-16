5ed944932f9fd401f1f4d08f56fa8ba729caf901
Kei-Taros <orz.www127@gmail.com>
2026-05-16 17:19:37 +0900

    feat: Iconを実装

--- src/lib/components/atoms/index.ts
+++ src/lib/components/atoms/index.ts
@@ -1,6 +1,7 @@
 export { default as Button } from './Button/Button.svelte';
 export { default as DataItem } from './DataItem/DataItem.svelte';
 export { default as Heading } from './Heading/Heading.svelte';
+export { default as Icon } from './Icon/Icon.svelte';
 export { default as Logo } from './Logo/Logo.svelte';
 export { default as SkillIcon } from './SkillIcon/SkillIcon.svelte';
 export { default as SnapDots } from './SnapDots/SnapDots.svelte';
